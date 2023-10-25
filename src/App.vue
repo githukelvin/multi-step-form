d29d5472ff6fb29526c79c6b55ca1ac8d8d00b18
githukelvin <kelvingithu019@gmail.com>
2023-10-25 06:46:29 +0300

    styling started and sidebar done

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <div class="parent">
+      <SideBarMen />
+      <router-view></router-view>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import SideBarMen from '@/components/SideBarMen.vue'
+</script>
+<!-- <style lang="scss" scoped>
+
+</style> -->
---
+++ src/components/SideBarMen.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="sidebar">
+    <ul>
+      <li>
+        <div class="list">
+          <div class="number">
+            <h2>1</h2>
+          </div>
+          <div class="listInfo">
+            <p>step 1</p>
+            <h1>your info</h1>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="list">
+          <div class="number">
+            <h2>2</h2>
+          </div>
+          <div class="listInfo">
+            <p>step 2</p>
+            <h1>select plan</h1>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="list">
+          <div class="number">
+            <h2>3</h2>
+          </div>
+          <div class="listInfo">
+            <p>step 3</p>
+            <h1>add-ons</h1>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="list">
+          <div class="number">
+            <h2>4</h2>
+          </div>
+          <div class="listInfo">
+            <p>step 4</p>
+            <h1>summary</h1>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup></script>
+
+<!-- <style lang="scss" scoped></style> -->
